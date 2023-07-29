ab93f44c453a9bcf53b8d303883eac2d1e081879
Zollo757347 <81338417+Zollo757347@users.noreply.github.com>
2023-07-29 14:55:44 +0800

    chore: add comments

--- src/calculator/Lexer.ts
+++ src/calculator/Lexer.ts
@@ -1,19 +1,28 @@
 import { Conveyor } from "./";
 import { binaryOperator, Token, TokenType, unaryOperator } from "../types";
 
+const operators = [...unaryOperator, ...binaryOperator];
+
 export class Lexer {
+  /** 用來儲存原始資料的結構 */
   private conveyor: Conveyor<string>;
+
+  /** 用來儲存解析後的單詞 */
   private tokens: Token[];
   
-
   constructor() {
     this.conveyor = new Conveyor();
     this.tokens = [];
   }
 
+  /** 分析字串的詞法 */
   public lex(str: string): Token[] {
     this.reset();
+    
+    // 將原始資料拆分為字元陣列
     this.conveyor.add(str.split(''));
+
+    // 逐字元分析詞法
     let current = this.conveyor.peek();
     while (!current.done) {
       const char = current.value;
@@ -27,22 +36,28 @@ export class Lexer {
       if (!success) throw new Error(`Invalid token ${char}`);
       current = this.conveyor.peek();
     }
-    return this.tokens;
+
+    const result = this.tokens;
+    return result;
   }
 
 
+  /** 清空容器 */
   private reset(): void {
     this.conveyor = new Conveyor();
     this.tokens = [];
   }
 
+  /** 分析接下來的原始資料的含意是否為空白，如是，則加入 `#tokens` 中 */
   private lexSpace(char: string): boolean {
     if (!Lexer.isSpace(char)) return false;
 
+    // 空白視為單詞之間的分隔
     this.conveyor.next();
     return true;
   }
   
+  /** 分析接下來的原始資料的含意是否為逗號，如是，則加入 `#tokens` 中 */
   private lexComma(char: string): boolean {
     if (!Lexer.isComma(char)) return false;
 
@@ -51,24 +66,28 @@ export class Lexer {
     return true;
   }
 
+  /** 分析接下來的原始資料的含意是否為運算元，如是，則加入 `#tokens` 中 */
   private lexOperator(char: string): boolean {
     if (!Lexer.isOperator(char)) return false;
 
+    let candidates = operators;
     let next = this.conveyor.peek();
     let operator = '';
     while (!next.done) {
       const temp = operator + next.value;
-      if (unaryOperator.every(o => !o.startsWith(temp)) && binaryOperator.every(o => !o.startsWith(temp))) break;
+      candidates = candidates.filter(o => o.startsWith(temp));
+      if (!candidates.length) break;
       operator = temp;
       this.conveyor.next();
       next = this.conveyor.peek();
     }
 
-    if (!operator.length) throw new Error('Invalid operator');
+    if (!operators.some(o => o === operator)) throw new Error('Invalid operator');
     this.tokens.push({ type: TokenType.Operator, value: operator });
     return true;
   }
 
+  /** 分析接下來的原始資料的含意是否為括號，如是，則加入 `#tokens` 中 */
   private lexParenthesis(char: string): boolean {
     if (!Lexer.isParenthesis(char)) return false;
 
@@ -77,6 +96,7 @@ export class Lexer {
     return true;
   }
 
+  /** 分析接下來的原始資料的含意是否為數字，如是，則加入 `#tokens` 中 */
   private lexNumber(char: string): boolean {
     if (!Lexer.isNumber(char)) return false;
 
@@ -95,6 +115,7 @@ export class Lexer {
     return true;
   }
 
+  /** 分析接下來的原始資料的含意是否為關鍵字，如是，則加入 `#tokens` 中 */
   private lexKeyword(char: string): boolean {
     if (!Lexer.isKeyword(char)) return false;
 
--- src/calculator/Parser.ts
+++ src/calculator/Parser.ts
@@ -3,34 +3,46 @@ import { BinaryOperator, ConstantExpression, Expression, FunctionExpression, Tok
 import { Util } from "../utils";
 
 export class Parser {
+  /** 用來儲存 lexer 解析後的單詞 */
   private conveyor: Conveyor<Token>;
+
+  /** 儲存表達式的 stack */
   private expressions: Stack<Expression>;
+
+  /** 儲存二元運算子的 stack */
   private operators: Stack<BinaryOperator>;
   
-
   constructor() {
     this.conveyor = new Conveyor();
     this.expressions = new Stack();
     this.operators = new Stack();
   }
 
+  /** 分析單詞的語法 */
   public parse(tokens: Token[]): Expression {
     this.reset();
     this.conveyor.add(tokens);
 
     const result = this.parseExpression();
+
+    // 在語法分析結束後，stack 不應留下任何物品
     if (this.expressions.size || this.operators.size) throw new Error('Invalid end');
+
+    // 在語法分析結束後，儲存單詞的容器不應留下任何物品
     if (!this.conveyor.peek().done) throw new Error('Extra tokens at tail');
+
     return result;
   }
 
 
+  /** 清空容器 */
   private reset() {
     this.conveyor = new Conveyor();
     this.expressions = new Stack();
     this.operators = new Stack();
   }
 
+  /** 遞迴地解析語法 */
   private parseExpression(): Expression {
     let current = this.conveyor.peek();
     if (current.done) throw new Error('Nothing to parse');
@@ -38,11 +50,12 @@ export class Parser {
     let exp: Expression | null = 
       this.parseParenthesis(current.value) ||
       this.parseNumber(current.value) ||
-      this.parseConstantKeyword(current.value) ||
-      this.parseFunctionKeyword(current.value) ||
+      this.parseConstant(current.value) ||
+      this.parseFunction(current.value) ||
       this.parseUnaryOperator(current.value);
     if (exp === null) throw new Error(`Invalid token ${current.value.value}`);
 
+    // 二元運算
     current = this.conveyor.peek();
     if (!current.done && Util.isBinaryOperator(current.value)) {
       const size = this.operators.size;
@@ -63,12 +76,20 @@ export class Parser {
     return exp;
   }
 
+  /**
+   * 往下解析括號表達式
+   * 
+   * `<括號表達式>` = `<左括號>` `<表達式>` `<右括號>`
+   */
   private parseParenthesis(token: Token): Expression | null {
+    // 左括號
     if (!Util.isOpenParenthesis(token)) return null;
     this.conveyor.next();
 
+    // 表達式
     const exp = this.parseExpression();
     
+    // 右括號
     const current = this.conveyor.peek();
     if (!current.done && !Util.isCloseParenthesis(current.value)) throw new Error('Parenthesis not closed');
     this.conveyor.next();
@@ -76,6 +97,7 @@ export class Parser {
     return exp;
   }
 
+  /** 往下解析數字 */
   private parseNumber(token: Token): number | null {
     if (!Util.isNumber(token)) return null;
 
@@ -86,31 +108,41 @@ export class Parser {
     return num;
   }
 
-  private parseConstantKeyword(token: Token): ConstantExpression | null {
+  /** 往下解析常數 */
+  private parseConstant(token: Token): ConstantExpression | null {
     if (!Util.isConstantKeyword(token)) return null;
 
     this.conveyor.next();
     return { c: token.value };
   }
 
-  private parseFunctionKeyword(token: Token): FunctionExpression | null {
+  /**
+   * 往下解析函數表達式
+   * 
+   * `<函數表達式>` = `<函數關鍵字>` `<左括號>` `<參數表達式>` `<右括號>`
+   */
+  private parseFunction(token: Token): FunctionExpression | null {
+    // 函數關鍵字
     if (!Util.isFunctionKeyword(token)) return null;
-      const func = token.value;
-      this.conveyor.next();
-      
-      let current = this.conveyor.peek();
-      if (!current.done && !Util.isOpenParenthesis(current.value)) throw new Error('Expecting parenthesis');
-      this.conveyor.next();
-      
-      const funcArgs = this.parseArguments();
-      
-      current = this.conveyor.peek();
-      if (!current.done && !Util.isCloseParenthesis(current.value)) throw new Error('Parenthesis not closed');
-      this.conveyor.next();
+    this.conveyor.next();
+    
+    // 左括號
+    let current = this.conveyor.peek();
+    if (!current.done && !Util.isOpenParenthesis(current.value)) throw new Error('Expecting parenthesis');
+    this.conveyor.next();
+    
+    // 參數表達式
+    const funcArgs = this.parseArguments();
+    
+    // 右括號
+    current = this.conveyor.peek();
+    if (!current.done && !Util.isCloseParenthesis(current.value)) throw new Error('Parenthesis not closed');
+    this.conveyor.next();
 
-      return { f: func, a: funcArgs };
+    return { f: token.value, a: funcArgs };
   }
 
+  /** 往下解析單元運算表達式 */
   private parseUnaryOperator(token: Token): UnaryExpression | null {
     if (!Util.isUnaryOperator(token)) return null;
 
@@ -118,19 +150,31 @@ export class Parser {
     return { o: token, v: this.parseExpression() };
   }
 
+  /**
+   * 往下解析參數表達式
+   * 
+   * `<參數表達式>` = `<表達式>` [...`<逗號>` `<表達式>`]
+   */
   private parseArguments(): Expression[] {
     const args: Expression[] = [];
+
+    // 表達式
     args.push(this.parseExpression());
 
+    // 逗號
     let current = this.conveyor.peek();
     while (!current.done && Util.isComma(current.value)) {
       this.conveyor.next();
+
+      // 表達式
       args.push(this.parseExpression());
       current = this.conveyor.peek();
     }
+
     return args;
   }
 
+  /** 依據優先序高於 `arg` 的暫存表達式合併 */
   private clearStack(arg?: BinaryOperator | number): void {
     let current = this.operators.peek();
     while (current && (typeof arg === 'number' ? this.operators.size - arg : Parser.comparePrecedence(current, arg)) > 0) {
@@ -147,6 +191,7 @@ export class Parser {
     }
   }
 
+  /** 判斷 `opr1` 相較於 `opr2` 是否有比較高的優先權，若有，則回傳正值，反之則回傳負值，若相等則回傳 0 */
   private static comparePrecedence(opr1: BinaryOperator, opr2?: BinaryOperator): number {
     return this.getPrecedence(opr1) - this.getPrecedence(opr2);
   }
